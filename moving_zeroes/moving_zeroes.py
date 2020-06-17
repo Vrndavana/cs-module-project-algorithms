cff594083a3cde8b34fe059f2135b694fa649f2a
Vrndavana <Vrmdavama.th@gmail.com>
2020-06-17 13:59:37 -0700

    Base Project

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,12 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+	# Your code here
+	count = arr.count(0)
+	for i in range(0, count):
+		arr.remove(0)
+		arr.append(0)
+	return arr # returns in 0.000s
 
 
 if __name__ == '__main__':
@@ -13,3 +16,10 @@ if __name__ == '__main__':
     arr = [0, 3, 1, 0, -2]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+
+# def moving_zeroes(arr):
+#     # Your code here
+#     for i in arr:
+#         if i == 0:
+#             arr.append(arr.pop(arr.index(0)))
+#     return arr
